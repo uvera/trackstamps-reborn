37574302f79d0469ad477a20fe52bcb031e4beb4
Dusan <dusanuveric@protonmail.com>
2024-09-01 13:55:44 +0200

    add test to ensure module is instantiated once
    Signed-off-by: Dusan <dusanuveric@protonmail.com>

--- spec/trackstamps/reborn_instance_spec.rb
+++ spec/trackstamps/reborn_instance_spec.rb
@@ -87,4 +87,12 @@ RSpec.describe Trackstamps::Reborn do
     instance = Trackstamps::Reborn[:inspected]
     expect(instance.inspect).to eq("Trackstamps::Reborn[:inspected]")
   end
+
+  it "only constructs module once" do
+    Trackstamps::Reborn[:once]
+    module_count = Trackstamps::Base.instance_variable_get(:@mixins).keys.length
+    Trackstamps::Reborn[:once]
+    new_count = Trackstamps::Base.instance_variable_get(:@mixins).keys.length
+    expect(module_count).to eq(new_count)
+  end
 end
